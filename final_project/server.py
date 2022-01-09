ce1d76f92c4fa4835b5131172ca843869aacf9bf
emmywhyte93 <emmanuel4sure93@gmail.com>
2022-01-09 16:56:42 +0100

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,24 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
+import json
 
-app = Flask("Web Translator", static_folder="static")
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    translated = translator.english_to_french(textToTranslate)
+    return translated
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    translated = translator.french_to_english(textToTranslate)
+    return translated
 
 @app.route("/")
 def renderIndexPage():
-    return render_template('index.html')
-
-@app.route("/english_to_french", methods=['GET'])
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    translation = translator.english_to_french(textToTranslate)
-    return translation
-   
-
-@app.route("/french_to_english", methods=['GET'])
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    translation = translator.french_to_english(textToTranslate)
-    return translation
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(host="0.0.0.0", port=8080)
